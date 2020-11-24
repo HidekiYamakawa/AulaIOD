f221aacd76bcb9643ea7e61c7e2d6550f8833862
Carlos Hideki Yamakawa <hideki.yamakawa@outlook.com>
2020-11-24 19:33:34 -0300

    Aula 09 - Notificações push

---
+++ app/src/main/java/com/umc/iod/aulaiod/repository/local/NotificacaoDAO.java
@@ -0,0 +1,24 @@
+package com.umc.iod.aulaiod.repository.local;
+
+
+import android.view.View;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.umc.iod.aulaiod.model.Notificacao;
+
+import java.util.List;
+
+@Dao
+public interface NotificacaoDAO {
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    public void inserir(Notificacao notificacao);
+
+    @Query("SELECT * FROM notificacao")
+    public LiveData<List<Notificacao>> listarTodos();
+}
